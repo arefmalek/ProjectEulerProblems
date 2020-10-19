bb18e547020150c1c69cf6d35ddfd26a4201f590
arefmalek <malek.aref1@gmail.com>
2020-10-18 16:10:03 -1000

    new idea for problem 7

--- Problem7/p7.py
+++ Problem7/p7.py
@@ -1,8 +1,12 @@
 def prime_number(number):
-    primes = []
-    starting_number = 2
 
-    for i in range(2,2):
+    for i in range(3, number / 2 +1, 2):
+        if (number % i == 0):
+            return False
+
+
+def through_primes(number):
+
 
 
 
