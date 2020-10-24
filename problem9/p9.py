b062f744da016e0a34f857c718e3153c54674cc6
arefmalek <malek.aref1@gmail.com>
2020-10-24 09:43:15 -1000

    31875000

--- problem9/p9.py
+++ problem9/p9.py
@@ -1,9 +1,4 @@
-def triplet(n):
-    for x in range(1, n):
-        for y in range((x+1), n):
-            for z in range((y+1), n):
-                if x ** 2 == y ** 2 + z ** 2:
-                    print(y, z, x)
-                    print('/' * 50)
+import numpy as np
 
-triplet(1000)
+golden = np.array([8,15,17])
+print(np.prod(golden*25))
