5eb5deaff2f405cfead43ff58186c4b078e2287f
arefmalek <malek.aref1@gmail.com>
2020-12-24 16:18:22 -0800

    solved p28 for odd numbers

--- problem28/p28.py
+++ problem28/p28.py
@@ -1,2 +1,11 @@
-def poggers(base_case, current = 1):
+def spiral(n):
+    if (n == 1):
+        return 1
+
+    sum = 0
+    for i in range(n**2 - 3 * (n-1), n**2 + 1, (n-1)):
+        sum += i
     
+    return spiral(n-2) + sum
+
+print(spiral(1001))
